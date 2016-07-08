47f370f410a6fdbc03a50335b123e56be3e77f5c
Your Name <you@example.com>
2016-07-07 21:32:23 -0300

    Commit

---
+++ src/main/java/com/mycompany/empleadomanager/modelo/main.java
@@ -0,0 +1,16 @@
+
+package com.mycompany.empleadomanager.modelo;
+
+
+public class main {
+    
+    boolean esFeliz;
+    
+    public static void main(String[] args){
+        Integer i = new Integer(2);
+        Integer i2 = new Integer(2);
+        Integer i3 = Integer.valueOf(2);
+        System.out.println("Fin del programa");
+    }
+    
+}
